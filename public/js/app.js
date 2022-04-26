849cc07f382389c494cf3a07c669ad4d77023a91
Jason Omac <68273592+jasonomacabrahan@users.noreply.github.com>
2022-04-26 09:03:16 +0800

    first commit

--- public/js/app.js
+++ public/js/app.js
@@ -40190,8 +40190,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\server0\www\02\laravel-vue-kanban-tutorial\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\server0\www\02\laravel-vue-kanban-tutorial\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! C:\server0\www\project\pm\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\server0\www\project\pm\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
